6f57047a7669e3e482f2dbb602db2fee3315b89d
nato000 <64704016+nato000@users.noreply.github.com>
2021-02-09 20:37:22 +0200

    8

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ init();
 
 function init() {
     console.log('Start Game!');
-   }
+}
 
 $btn1.addEventListener('click', function () {
-    attack(20,20, 'smash')
+    attack(20, 20, 'smash')
 })
 $btn2.addEventListener('click', function () {
-    attack(40, 40, 'blast')
+    attack(30, 30, 'blast')
 })
 
 const picachu = {
-    name: 'Picachu',
+    name: 'picachu',
     defaultHP: 100,
     damageHP: 100,
     elHP: document.getElementById('health-picachu'),
@@ -25,7 +25,7 @@ const picachu = {
 }
 
 const charmander = {
-    name: 'Charmander',
+    name: 'charmander',
     defaultHP: 100,
     damageHP: 100,
     elHP: document.getElementById('health-charmander'),
@@ -36,29 +36,55 @@ const charmander = {
 
 function random(num) {
     return Math.ceil(Math.random() * num);
+
 }
 
 function attack(cdamage, pdamage, atack) {
     console.log(atack);
     picachu.changeHP(random(pdamage));
-    charmander.changeHP(random(cdamage));    
+    charmander.changeHP(random(cdamage));
     picachu.renderHP();
     charmander.renderHP();
+    logOut();
+}
+
+function logOut(first, second) {
+    const { name: name1, damageHP, defaultHP } = picachu;
+    const { name: name2 } = charmander;
+    const logs = [
+        `${name1} вспомнил что-то важное, но неожиданно ${name2}, не помня себя от испуга, ударил в предплечье врага.[${damageHP} / ${defaultHP} ]  `,
+        `${name1} поперхнулся, и за это ${name2} с испугу приложил прямой удар коленом в лоб врага. [${damageHP} / 100 ]`,
+        `${name1} забылся, но в это время наглый ${name2}, приняв волевое решение, неслышно подойдя сзади, ударил. [${damageHP} / ${defaultHP} ]`,
+        `${name1} пришел в себя, но неожиданно ${name2} случайно нанес мощнейший удар. [${damageHP} / ${defaultHP} ]`,
+        `${name1} поперхнулся, но в это время ${name2} нехотя раздробил кулаком <вырезанно цензурой> противника. [${damageHP} / ${defaultHP} ]`,
+        `${name1} удивился, а ${name2} пошатнувшись влепил подлый удар. [${damageHP} / ${defaultHP} ]`,
+        `${name1} высморкался, но неожиданно ${name2} провел дробящий удар. [${damageHP} / ${defaultHP}] `,
+        `${name1} пошатнулся, и внезапно наглый ${name2} беспричинно ударил в ногу противника [${damageHP} / ${defaultHP}]`,
+        `${name1} расстроился, как вдруг, неожиданно ${name2} случайно влепил стопой в живот соперника. [${damageHP} / ${defaultHP} ]`,
+        `${name1} пытался что-то сказать, но вдруг, неожиданно ${name2} со скуки, разбил бровь сопернику. [${damageHP} / ${defaultHP} ]`,
+    ];
+    const $logs = document.querySelector('#logs');
+    const $p = document.createElement('p');
+    $p.innerText = logs[random(logs.length) - 1];
+    $logs.insertBefore($p, $logs.children[0]);
 }
 
 function renderHP() {
     this.elHP.innerText = this.damageHP + '/' + this.defaultHP;
     this.elProgressbar.style.width = this.damageHP + '%';
+
 }
 
 function changeHP(count) {
+    const { name } = this;
     if (this.damageHP < count) {
         this.damageHP >= 0,
             $btn1.disabled = true,
             $btn2.disabled = true,
-            alert('Для ' + this.name + ' GAME OVER!'),
+            alert('Для ' + name + ' GAME OVER!'),
             location.reload()
     } else {
         this.damageHP -= count;
     }
+
 }
